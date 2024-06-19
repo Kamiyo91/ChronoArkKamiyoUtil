24e33ebb5036b81ced5566c71b70a10d31bbec55
Kamiyo <kamiyo.k91@gmail.com>
2024-06-20 01:55:55 +0200

    Renaming

--- MainHarmonyPatch.cs
+++ MainHarmonyPatch.cs
@@ -14,7 +14,7 @@ namespace _1ChronoArkKamiyoUtil
         [HarmonyPatch(typeof(MasterSDMap), nameof(MasterSDMap.PartySpine))]
         [HarmonyPatch(typeof(MasterSDMap2), nameof(MasterSDMap2.PartySpine))]
         [HarmonyPatch(typeof(MasterSDMap3), nameof(MasterSDMap2.PartySpine))]
-        public static void RootStoryScript_Start(object __instance, int Num, string KeyData)
+        public static void InstanceMap_Start(object __instance, int Num, string KeyData)
         {
             if (!ModManager.IsModAddedGDE(PlayData.TSavedata.Party[Num].KeyData)) return;
             var tryGetDialogs = KamiyoGlobalModParameters.DialogueTrees.TryGetValue(KeyData, out var dialogueItem);
@@ -30,19 +30,19 @@ namespace _1ChronoArkKamiyoUtil
                     break;
                 case ClockTowerMap instance:
                     KamiyoArtUtil.PrepareSkin(instance, Num, KeyData);
-                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(RootStoryScript));
+                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(ClockTowerMap));
                     if (item != null) KamiyoArtUtil.PrepareDialogue(instance, Num, KeyData, item.DialogueTree);
                     else KamiyoArtUtil.PrepareBlankDialogue(instance, Num, KeyData);
                     break;
                 case KingRoomScript instance:
                     KamiyoArtUtil.PrepareSkin(instance, Num, KeyData);
-                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(RootStoryScript));
+                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(KingRoomScript));
                     if (item != null) KamiyoArtUtil.PrepareDialogue(instance, Num, KeyData, item.DialogueTree);
                     else KamiyoArtUtil.PrepareBlankDialogue(instance, Num, KeyData);
                     break;
                 case MasterSDMap instance:
                     KamiyoArtUtil.PrepareSkin(instance, Num, KeyData);
-                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(RootStoryScript));
+                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(MasterSDMap));
                     if (item != null)
                         KamiyoArtUtil.PrepareDialogue(instance, Num, KeyData, item.DialogueTree, item.DialogueTreeFail,
                             item.DialogueTreeShort);
@@ -50,14 +50,14 @@ namespace _1ChronoArkKamiyoUtil
                     break;
                 case MasterSDMap2 instance:
                     KamiyoArtUtil.PrepareSkin(instance, Num, KeyData);
-                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(RootStoryScript));
+                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(MasterSDMap2));
                     if (item != null)
                         KamiyoArtUtil.PrepareDialogue(instance, Num, KeyData, item.DialogueTree, item.DialogueTreeFail);
                     else KamiyoArtUtil.PrepareBlankDialogue(instance, Num, KeyData);
                     break;
                 case MasterSDMap3 instance:
                     KamiyoArtUtil.PrepareSkin(instance, Num, KeyData);
-                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(RootStoryScript));
+                    item = dialogueItem.FirstOrDefault(x => x.DialogueName == nameof(MasterSDMap3));
                     if (item != null)
                         KamiyoArtUtil.PrepareDialogue(instance, Num, KeyData, item.DialogueTree, item.DialogueTreeFail);
                     else KamiyoArtUtil.PrepareBlankDialogue(instance, Num, KeyData);
